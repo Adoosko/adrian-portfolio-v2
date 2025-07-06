29f0ec0bf4e37bba40b00e9c72a6c25ebdf0b906
Adoosko <adoosdeveloper@gmail.com>
2025-07-06 13:13:48 +0200

    fix: Disable Next.js compression to enable Vercel's

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
+  compress: false,
 };
  
 const withNextIntl = createNextIntlPlugin();
